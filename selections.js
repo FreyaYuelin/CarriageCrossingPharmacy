adc4f0fcf67e2fcddb698b0001003dd60dad6c7a
ZI Xuan Zheng <nexusfanboy@ZIs-MacBook-Pro.local>
2020-05-19 11:29:00 -0400

    added some initial DOM interactions

--- selections.js
+++ selections.js
@@ -6,6 +6,8 @@ selectionForm.addEventListener('submit', submitFunc);
 const username = document.getElementById('username');
 username.innerHTML = localStorage.getItem('uname');
 
+const submitButton = document.querySelector('#submit-button');
+
 myStorage = window.localStorage;
 
 log(localStorage.getItem('uname'));
@@ -16,3 +18,17 @@ function submitFunc(e) {
     window.location.href = "index.html";
 }
 
+function addToProductsList(e) {
+    e.preventDefault();
+    selectionForm.insertBefore(e.target.parentElement, submitButton);
+    let textfield = e.target.parentElement.querySelector('#item-input');
+    textfield.style.visibility = 'visible';
+    e.target.style.visibility = 'hidden';
+
+
+    
+
+}
+
+const addButton = document.querySelector('#add-button');
+addButton.addEventListener('click', addToProductsList);
