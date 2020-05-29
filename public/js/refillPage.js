1a75f376a31aaefcf2f5c96de669159032b5b016
ZI Xuan Zheng <nexusfanboy@ZIs-MacBook-Pro.local>
2020-05-29 18:06:24 -0400

    sql now known to be working?(query code currently commented out)

--- public/js/refillPage.js
+++ public/js/refillPage.js
@@ -1,42 +1,53 @@
 const log = console.log;
 
+var myStorage = window.localStorage;
+
 const refillLoginForm = document.getElementById('refillLoginForm');
 // refillLoginForm.addEventListener('submit', logIn);
 
 const quickRefill = document.querySelector('#quickRefill');
 quickRefill.addEventListener('click', foo);
 
+const quickEmail = document.querySelector('#quick-email');
+
+
 const user1 = {
     uname: "david",
     pwd: "liu"
 }
 
- function foo(e) {
-    e.preventDefault();
-    const url = "/index/auth";
-    const data = {
-        uname: document.getElementById("uname").value,
-        pwd: document.getElementById("pwd").value
-    }
-    const request = new Request(url, {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: {
-            'Accept': 'application/json, text/plain, */*',
-            'Content-Type': 'application/json'
-        },
-    });
-    fetch(request)
-    .then((res) => {
-        log(res.status)
-        if (res.status === 200) {
-        } else {
-            alert("Incorrect user name or password")
-        }
-    }).catch((error) => {
-        console.error(error)
-    })
- }
+function foo(e) {
+    //e.preventDefault();
+    localStorage.setItem('email', quickEmail.value)
+    log(localStorage.getItem('email'))
+}
+
+ // function foo(e) {
+ //    e.preventDefault();
+ //    const url = "/index/auth";
+ //    const data = {
+ //        uname: document.getElementById("uname").value,
+ //        pwd: document.getElementById("pwd").value
+ //    }
+ //    const request = new Request(url, {
+ //        method: "POST",
+ //        body: JSON.stringify(data),
+ //        headers: {
+ //            'Accept': 'application/json, text/plain, */*',
+ //            'Content-Type': 'application/json'
+ //        },
+ //    });
+ //    fetch(request)
+ //    .then((res) => {
+ //        log(res.status)
+ //        if (res.status === 200) {
+ //        } else {
+ //            alert("Incorrect user name or password")
+ //        }
+ //    }).catch((error) => {
+ //        console.error(error)
+ //    })
+ // }
 
 
 // function logIn(e) {
--- server.js
+++ server.js
@@ -7,6 +7,15 @@ const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 const path = require('path');
 
+const { Pool, Client } = require('pg')
+const pool = new Pool({
+  user: 'nexus',
+  host: 'localhost',
+  database: 'api',
+  password: 'password',
+  port: 5432,
+})
+
 
 const app = express();
 
@@ -21,10 +30,6 @@ app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended:true }));
 
-const localhost ="http://localhost:3000";
-// const router = express.Router();
-// app.use('/', router);
-
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/refillPage.html');
@@ -35,16 +40,28 @@ app.get('/calendar', (req, res) => {
 })
 
 
-
 app.post('/index/auth', (req, res) => {
-    if (req.body.uname === "user" && req.body.pwd === "user") {
-        return res.redirect('/calendar');
-    } else {
-        res.status(400).sendFile(__dirname + '/public/refillPage.html');
-    }
-    //next();
+    return res.redirect('/calendar');
+    // res.status(400).redirect('/');
+    // if (req.body.uname === "user" && req.body.pwd === "user") {
+    //     return res.redirect('/calendar');
+    // } else {
+    //     res.status(400).redirect('/');
+    // }
 
 })
+// var queryString = "INSERT INTO appointments(email, date, start, finish, option, considerations, address, phone)values('john123@gmail.com', '050220', '13:30', '14:00', '2', 'no special considerations', '6955 Fielding', '123-456-7890')"
+// pool.query(queryString, (err, resp) => {
+//     log(err, resp);
+//     pool.end();
+// })
+
+  //[req.body.email, req.body.name, req.body.start, req.body.finish, req.body.option, req.body.considerations, req.body.address, req.body.phone], (err, resp) => {
+app.post('/appointment', (req, res) => {
+  //var queryString = 'INSERT INTO appointments(email, date, start, finish, option, considerations, address, phone, lastname) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)'
+
+})
+
 
 
 
