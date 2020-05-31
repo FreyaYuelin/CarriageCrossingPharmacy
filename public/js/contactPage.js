b223b89c12d260ae913b5710437d4dbdec86a6b9
Jasonczx <55595850+Jasonczx@users.noreply.github.com>
2020-05-31 10:54:47 -0400

    Update contactPage.js

--- public/js/contactPage.js
+++ public/js/contactPage.js
@@ -1,69 +1,18 @@
-* {box-sizing: border-box;}
-ul {list-style-type: none;}
-body {font-family: Verdana, sans-serif;}
+const sendButton = document.querySelector("#send-button");
 
-#bold {
-  font-size: 16;
-  font-weight: bold;
-  margin-right: 7px;
+const phone = document.querySelector("#phone-number");
+
+const email = document.querySelector("#email");
+
+const message = document.querySelector("#message");
+
+const name = document.querySelector("#name");
+
+const suggest = document.querySelector(".suggestion-box");
+suggest.addEventListener("submit", goto);
+
+function goto(e) {
+	e.preventDefault();
+	console.log(phone.value, email.value, message.value, name.value);
+	
 }
-
-.contact-box {
-  width: 35%;
-  background-color: yellow;
-  margin: 50px 40px;
-  float: left;
-  min-height: 300px;
-}
-
-.contact-box p {
-  margin-left: 5px;
-}
-
-.suggestion-box {
-  min-width: 35%;
-  background-color: yellow;
-  margin: 50px 40px;
-  float: left;
-  min-height: 300px;
-}
-
-.suggestion-box input {
-  margin-left: 5px;
-  display: block;
-  border-radius: 3%;
-  padding: 5px 5px 5px;
-  margin-top: 9px;
-  width: 70%;
-}
-
-#message {
-  margin: 12px 5px 0px;
-}
-
-#send-button {
-  border-radius: 7%;
-  font-size: 16;
-  margin-bottom: 5px;
-  background-color: red;
-
-}
-
-.center-box {
-  /*background-color: grey;*/
-  width: 80%;
-  height: 100%;
-}
-
-textarea {
-  resize: none;
-}
-
-
-
-
-
-
-
-
-
