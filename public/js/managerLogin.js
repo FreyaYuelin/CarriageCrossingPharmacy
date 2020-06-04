851e9bf776c6dede7ba8a8308a5184afa3e372e5
ZI Xuan Zheng <nexusfanboy@ZIs-MacBook-Pro.local>
2020-06-04 18:58:13 -0400

    possibly final update

---
+++ public/js/managerLogin.js
@@ -0,0 +1,31 @@
+const log = console.log;
+
+var myStorage = window.localStorage;
+
+// const form = document.querySelector('#refillLoginForm');
+// form.addEventListener('submit', authenticate);
+
+// const uname = document.querySelector('#username');
+// const pwd = document.querySelector('#pwd');
+
+
+// function authenticate() { 
+// 	if (uname.value === "user" && pwd.value === "user") {
+// 		alert(123)
+// 		const url = '/managerAuthenticated';
+// 		const request = new Request(url, {
+//       		method: "GET", 
+//       		headers: {
+//         		'Accept': 'application/json, text/plain, */*',
+//         		'Content-Type': 'application/json'
+//     		}
+//     	});
+//     	fetch(request);
+// 	}
+// 	else {
+// 		alert("Wrong username or password")
+// 	}
+// }
+
+
+
